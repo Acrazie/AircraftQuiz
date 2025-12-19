04a60a7c79cbbb65f39b282646b42a9a2730063e
Acrazie <mayeul.desbazeille@gmail.com>
2025-12-19 23:05:18 +0100

    [+] adding register form

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import LoginForm from "@/components/ui/LoginForm";
+import RegisterForm from "@/components/ui/RegisterForm";
 const Profile = () => {
     return (
         <div className="flex-1 h-full flex flex-col justify-center items-center">
             <LoginForm />
+            {/* <RegisterForm /> */}
         </div>
     );
 };
