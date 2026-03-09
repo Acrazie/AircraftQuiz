b95b24f620a781c2c550612b79b8d1d7fbf047c5
Acrazie <mayeul.desbazeille@gmail.com>
2026-03-09 16:45:01 +0100

    Remove Link wrapper from 3D HoverCard

--- client/src/components/ui/3dhover-card.jsx
+++ client/src/components/ui/3dhover-card.jsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const HoverCard = ({ children }) => {
-    return (
-        <Link to='/aircraft-quiz'>
-            <div className="hover-3d">
-                {/* content */}
-                {children}
-                {/* 8 empty divs needed for the 3D effect */}
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </Link>
-    );
+  return (
+    <div className="hover-3d">
+      {/* content */}
+      {children}
+      {/* 8 empty divs needed for the 3D effect */}
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default HoverCard;
